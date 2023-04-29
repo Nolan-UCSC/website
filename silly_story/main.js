16dbda8aa38bebd90fa341a41a7ff3e1de017eed
Nolan Jensen <130103322+Nolan-UCSC@users.noreply.github.com>
2023-04-28 21:36:49 -0700

    making it less offenseive

--- silly_story/main.js
+++ silly_story/main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: looked crazy, and it was a hot day.';
 const insertX = ['Andrew Moshkovich', 'Jim Whitehead', 'Henry Cowell'];
 const insertY = ['Basken Engineering', 'Room 055', 'UCSC'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a banana slug and crawled away'];
